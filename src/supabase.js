795cdb39be97fb1dceec5325c690a7e5572bab0a
Hilal ahmad <86137247+hilalahmad32@users.noreply.github.com>
2022-03-03 02:06:55 -0800

    change in supabase

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,5 @@
 
 import { createClient } from '@supabase/supabase-js'
-const url = "https://hnodwvzrqawesibnayjf.supabase.co";
-const key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhub2R3dnpycWF3ZXNpYm5heWpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDYyODg2MDcsImV4cCI6MTk2MTg2NDYwN30.YHUPjTf2roloInpjtALqDohJhGDTQhpqP83uB2BdPeQ"
-
+const url = "";
+const key = "";
 export const supabase = createClient(url, key);
